9dabc1b5a371f16952074b3d31bc4d8dae4bef03
Ned Batchelder <ned@nedbatchelder.com>
2017-07-08 08:50:59 -0400

    Class-based drawing

--- three_stars.py
+++ three_stars.py
@@ -9,74 +9,85 @@ from euclid import Line, Point
 from path_tiler import PathTiler
 from path_tiler import combine_paths, replay_path, path_in_box
 
+SQRT2 = math.sqrt(2)
+SQRT3 = math.sqrt(3)
 
 DWGW = 800
 TILEW = int(DWGW/5)
 LINE_WIDTH = TILEW/12
 
-def draw_tile(dwg, args):
-    TILEW, = args
-    OFFSET = TILEW/8
-    top = Point(0, 0)
-    bottom = Point(0, -TILEW)
-    belly = (TILEW * math.sqrt(3) / 4, -TILEW*.75)
-    border_side = Line(top, bottom)
-    border_shoulder = Line(top, belly)
-    border_foot = Line(belly, bottom)
+class Draw:
+    def __init__(self, tilew):
+        self.tilew = tilew
 
-    side_line = border_side.offset(-OFFSET)
-    shoulder_line = border_shoulder.offset(OFFSET)
-    foot_line = border_foot.offset(OFFSET)
+    def three_points(self):
+        self.top = Point(0, 0)
+        self.bottom = Point(0, -self.tilew)
+        self.belly = Point(self.tilew * SQRT3 / 4, -self.tilew * .75)
 
-    with dwg.saved():
-        dwg.translate(*belly)
-        dwg.rotate(30)
-        snip_top = dwg.in_device(0, OFFSET * math.sqrt(2))
-        snip_bottom = dwg.in_device(-OFFSET * math.sqrt(2), 0)
+    def draw_tile(self, dwg, args):
+        self.three_points()
+        border_side = Line(self.top, self.bottom)
+        border_shoulder = Line(self.top, self.belly)
+        border_foot = Line(self.belly, self.bottom)
 
-    snip_top = dwg.in_user(*snip_top)
-    snip_bottom = dwg.in_user(*snip_bottom)
-    snip_line = Line(snip_top, snip_bottom)
+        offset = self.tilew/8
+        side_line = border_side.offset(-offset)
+        shoulder_line = border_shoulder.offset(offset)
+        foot_line = border_foot.offset(offset)
 
-    far_left = Point(-TILEW * math.sqrt(3) / 2, -TILEW / 2)
-    shoulder_limit = Line(far_left, top)
-    shoulder_limit = shoulder_limit.offset(OFFSET)
+        with dwg.saved():
+            dwg.translate(*self.belly)
+            dwg.rotate(30)
+            snip_top = dwg.in_device(0, offset * SQRT2)
+            snip_bottom = dwg.in_device(-offset * SQRT2, 0)
 
-    far_right = Point(TILEW * math.sqrt(3) / 2, 0)
-    side_limit = Line(top, far_right)
-    side_limit = side_limit.offset(OFFSET)
+        snip_top = dwg.in_user(*snip_top)
+        snip_bottom = dwg.in_user(*snip_bottom)
+        snip_line = Line(snip_top, snip_bottom)
 
-    side_top = side_line.intersect(side_limit)
-    side_bottom = side_line.intersect(border_foot)
-    dwg.move_to(*side_top)
-    dwg.line_to(*side_bottom)
+        far_left = Point(-self.tilew * SQRT3 / 2, -self.tilew / 2)
+        shoulder_limit = Line(far_left, self.top)
+        shoulder_limit = shoulder_limit.offset(offset)
 
-    shoulder_top = shoulder_line.intersect(shoulder_limit)
-    shoulder_bottom = shoulder_line.intersect(snip_line)
-    dwg.move_to(*shoulder_top)
-    dwg.line_to(*shoulder_bottom)
+        far_right = Point(self.tilew * SQRT3 / 2, 0)
+        side_limit = Line(self.top, far_right)
+        side_limit = side_limit.offset(offset)
 
-    foot_top = foot_line.intersect(snip_line)
-    foot_bottom = foot_line.intersect(border_side)
-    dwg.move_to(*foot_top)
-    dwg.line_to(*foot_bottom)
+        side_top = side_line.intersect(side_limit)
+        side_bottom = side_line.intersect(border_foot)
+        dwg.move_to(*side_top)
+        dwg.line_to(*side_bottom)
 
-    dwg.move_to(*snip_top)
-    dwg.line_to(*foot_top)
-    dwg.move_to(*shoulder_bottom)
-    dwg.line_to(*snip_bottom)
+        shoulder_top = shoulder_line.intersect(shoulder_limit)
+        shoulder_bottom = shoulder_line.intersect(snip_line)
+        dwg.move_to(*shoulder_top)
+        dwg.line_to(*shoulder_bottom)
 
-    if 0:   # Outline the base triangle
-        dwg.move_to(*top)
-        dwg.line_to(*bottom)
-        dwg.line_to(*belly)
+        foot_top = foot_line.intersect(snip_line)
+        foot_bottom = foot_line.intersect(border_side)
+        dwg.move_to(*foot_top)
+        dwg.line_to(*foot_bottom)
+
+        dwg.move_to(*snip_top)
+        dwg.line_to(*foot_top)
+        dwg.move_to(*shoulder_bottom)
+        dwg.line_to(*snip_bottom)
+
+    def draw_triangle(self):
+        self.three_points()
+        dwg.move_to(*self.top)
+        dwg.line_to(*self.bottom)
+        dwg.line_to(*self.belly)
         dwg.close_path()
+
 
 dwg = Drawing(DWGW, DWGW, bg=(.85, .85, .85))
 pt = PathTiler()
+draw = Draw(TILEW)
 
 if 1:
-    pt.tile_p6m(draw_tile, dwg.get_size(), TILEW, args=(TILEW,))
+    pt.tile_p6m(draw.draw_tile, dwg.get_size(), TILEW)
 else:   # Draw one triangle
     pt.translate(400, 400)
     pt.scale(2, 2)
