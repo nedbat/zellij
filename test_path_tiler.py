bf85a04f2040d40fb548948668a4e67b825084c4
Ned Batchelder <ned@nedbatchelder.com>
2017-07-16 13:24:49 -0400

    More test data for combine_paths

--- test_path_tiler.py
+++ test_path_tiler.py
@@ -1,7 +1,10 @@
+"""Test path_tiler.py"""
+
+import collections
 import random
 
 from hypothesis import given
-from hypothesis.strategies import lists, randoms, composite
+from hypothesis.strategies import lists, randoms, composite, one_of
 
 from euclid import Point
 from hypo_helpers import points
@@ -114,9 +117,21 @@ def num_segments(paths):
     """How many individual line segments are in these paths?"""
     return sum(len(p)-1 for p in paths)
 
+def endpoints(paths):
+    """The list of endpoints of the paths."""
+    # Be careful to preserve duplicates from different paths, but avoid them
+    # in the same path.
+    endpoints = []
+    for path in paths:
+        endpoints.append(path[0])
+        if path[0] != path[-1]:
+            endpoints.append(path[-1])
+    return endpoints
+
 @composite
-def combinable_paths(draw):
-    path_points = draw(lists(points, min_size=3, max_size=200, unique_by=tuple))
+def combinable_paths_no_loops(draw):
+    """Makes varying-length paths, but no loops."""
+    path_points = draw(lists(points, min_size=2, max_size=200, unique_by=tuple))
     rand = draw(randoms())
 
     paths = [[]]
@@ -137,20 +152,49 @@ def combinable_paths(draw):
     rand.shuffle(paths)
     return paths
 
-@given(combinable_paths())
+@composite
+def combinable_paths_maybe_loops(draw):
+    """Makes single-segment paths, with loops a possibility."""
+    endpoints = draw(lists(points, min_size=2, max_size=200, unique_by=tuple))
+    rand = draw(randoms())
+    paths = set()
+    point_use = collections.defaultdict(int)
+
+    target_number = len(endpoints) / 3
+    if target_number < 1:
+        target_number = 1
+
+    while len(paths) < target_number:
+        # Choose two points at random from the possible endpoints, and make a
+        # segment.
+        a, b = rand.sample(endpoints, k=2)
+        if (a, b) in paths:
+            continue
+        paths.add((a, b))
+
+        # Track how many times the points have been used.
+        point_use[a] += 1
+        point_use[b] += 1
+
+        # Any point in two segments is no longer a candidate as an endpoint.
+        if point_use[a] == 2:
+            endpoints.remove(a)
+        if point_use[b] == 2:
+            endpoints.remove(b)
+
+    return paths
+
+@given(one_of(combinable_paths_no_loops(), combinable_paths_maybe_loops()))
 def test_combine_paths(paths):
     combined = combine_paths(paths)
-
-    print(f"p: {paths}")
-    print(f"c: {combined}")
 
     # Property: the points in the combined paths should all have been in the
     # original paths.
     assert point_set(paths) == point_set(combined)
 
     # Property: the combined paths should have no duplicate endpoints.
-    endpoints = [path[i] for path in combined for i in [0, -1]]
-    assert len(endpoints) == len(set(endpoints))
+    the_ends = endpoints(combined)
+    assert len(the_ends) == len(set(the_ends))
 
     # Property: the combined paths should have the same number of segments as
     # the original paths.
