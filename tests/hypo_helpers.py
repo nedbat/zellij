6573d4402c70c76b57bf6b6ad835f055555379c9
Ned Batchelder <ned@nedbatchelder.com>
2017-07-25 16:08:56 -0400

    No need to exclude nan and inf if there are limits

--- tests/hypo_helpers.py
+++ tests/hypo_helpers.py
@@ -7,7 +7,7 @@ from zellij.euclid import Point
 LIMIT = 10000
 
 # Numbers that are reasonable for Zellij.
-f = floats(min_value=-LIMIT, max_value=LIMIT, allow_nan=False, allow_infinity=False)
+f = floats(min_value=-LIMIT, max_value=LIMIT)
 i = integers(min_value=-LIMIT, max_value=LIMIT)
 
 # Points, on an integer grid to avoid some hairiness with pathological floats.
