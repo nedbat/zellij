b04bc96651462c34ff31b16c62ea4bb43aa267a1
Ned Batchelder <ned@nedbatchelder.com>
2017-08-02 07:37:18 -0400

    Separate the different kinds of drawings

---
+++ bin/talk_pictures.py
@@ -0,0 +1,89 @@
+import math
+
+from zellij.color import random_color, CasaCeramica
+from zellij.drawing import Drawing
+from zellij.path_tiler import combine_paths, offset_path, PathTiler
+
+from zellij.design.threestars import ThreeStarsDesign
+
+SQRT3 = math.sqrt(3)
+
+
+def draw_it(TILEW, dwg, combined=True, fat=True, color=(0, 0, 0), line_width=2, offset=None):
+    pt = PathTiler()
+    draw = ThreeStarsDesign(TILEW)
+    draw.draw(pt, dwg.get_size(), TILEW)
+    paths = pt.paths
+    if combined:
+        paths = combine_paths(pt.paths)
+    if offset is not None:
+        paths = [offset_path(p, offset) for p in paths]
+
+    if fat:
+        LINE_WIDTH = TILEW / 12
+        styles = [
+            (LINE_WIDTH, (0, 0, 0)),
+            (LINE_WIDTH*.7, (1, 1, 1)),
+        ]
+    else:
+        styles = [(line_width, color)]
+    dwg.multi_stroke(paths, styles)
+
+
+DWGW = 800
+
+def talk_pictures():
+    size = (883, 683)
+    TILEW = int(DWGW/3)
+
+    dwg = Drawing(*size, bg=(.85, .85, .85))
+    draw_it(TILEW, dwg)
+    dwg.write_to_png('three_stars_0.png')
+
+
+    dwg = Drawing(*size)
+    draw_it(TILEW, dwg, fat=False)
+    dwg.write_to_png('three_stars_1_thin.png')
+
+
+    dwg = Drawing(*size)
+    draw_it(TILEW, dwg, fat=False, color=(.8, .8, .8))
+
+    pt = PathTiler()
+    draw = ThreeStarsDesign(TILEW)
+    pt.tile_p6m(draw.draw_triangle, dwg.get_size(), TILEW)
+    with dwg.style(rgb=(1, 0, 0), width=2, dash=[5, 5]):
+        pt.replay_paths(dwg)
+        dwg.stroke()
+
+    pt = PathTiler()
+    pt.translate(2 * TILEW * SQRT3 / 2, TILEW)
+    pt.reflect_xy(0, 0)
+    draw.draw_tile(pt)
+    with dwg.style(rgb=(0, 0, 0), width=6):
+        pt.replay_paths(dwg)
+        dwg.stroke()
+
+    dwg.write_to_png('three_stars_2_lined.png')
+
+
+    dwg = Drawing(*size)
+    draw_it(TILEW, dwg, fat=False, color=random_color, combined=False, line_width=8)
+    dwg.write_to_png('three_stars_3_chaos.png')
+
+
+    dwg = Drawing(*size)
+    draw_it(TILEW, dwg, fat=False, color=random_color, combined=True, line_width=8)
+    dwg.write_to_png('three_stars_4_joined.png')
+
+def final():
+    TILEW = int(DWGW/5)
+
+    dwg = Drawing(DWGW, DWGW, bg=(.85, .85, .85))
+    draw_it(TILEW, dwg)
+    dwg.write_to_png('three_stars_final.png')
+
+
+if __name__ == '__main__':
+    talk_pictures()
+    final()
--- bin/three_stars.py
+++ bin/three_stars.py
@@ -1,141 +1,40 @@
-import itertools
-
-from zellij.color import random_color, CasaCeramica
 from zellij.drawing import Drawing
-from zellij.path_tiler import (
-    combine_paths, replay_path, path_in_box, offset_path,
-    PathTiler,
-)
+from zellij.path_tiler import combine_paths, replay_path, PathTiler
 from zellij.strap import strapify
 
 from zellij.design.threestars import ThreeStarsDesign
 
 
-def draw_it(TILEW, dwg, combined=True, fat=True, color=(0, 0, 0), line_width=2, offset=None):
-    pt = PathTiler()
-    draw = ThreeStarsDesign(TILEW)
-    draw.draw(pt, dwg.get_size(), TILEW)
-    paths = pt.paths
-    if combined:
-        paths = combine_paths(pt.paths)
-    if offset is not None:
-        paths = [offset_path(p, offset) for p in paths]
-
-    if fat:
-        LINE_WIDTH = TILEW / 12
-        styles = [
-            (LINE_WIDTH, (0, 0, 0)),
-            (LINE_WIDTH*.7, (1, 1, 1)),
-        ]
-    else:
-        styles = [(line_width, color)]
-    dwg.multi_stroke(paths, styles)
-
-
-if 0:
-    paths_in_box = [path for path in paths if path_in_box(path, (0, 0), (DWGW, DWGW))]
-    drawn = set()
-    colors = iter(itertools.cycle([
-        CasaCeramica.DarkGreen,
-        CasaCeramica.Yellow,
-        CasaCeramica.Red,
-        CasaCeramica.NavyBlue,
-    ]))
-    for path in paths_in_box:
-        if len(path) not in drawn:
-            dwg.set_source_rgb(*next(colors))
-            dwg.set_line_width(LINE_WIDTH*.7)
-            replay_path(path, dwg)
-            dwg.stroke()
-            drawn.add(len(path))
-
 DWGW = 800
 
-def talk_pictures():
-    size = (883, 683)
-    TILEW = int(DWGW/3)
+TILEW = int(DWGW/3)
+if 0:
+    strap_kwargs = dict(width=TILEW / 60, random_factor=4.9)
+else:
+    strap_kwargs = dict(width=TILEW / 15, random_factor=0)
 
-    dwg = Drawing(*size, bg=(.85, .85, .85))
-    draw_it(TILEW, dwg)
-    dwg.write_to_png('three_stars_0.png')
+pt = PathTiler()
+draw = ThreeStarsDesign(TILEW)
+draw.draw(pt, (DWGW, DWGW), TILEW)
+paths = pt.paths
+paths = combine_paths(pt.paths)
+paths = [tuple(path) for path in paths]
 
+straps = strapify(paths, **strap_kwargs)
 
-    dwg = Drawing(*size)
-    draw_it(TILEW, dwg, fat=False)
-    dwg.write_to_png('three_stars_1_thin.png')
+dwg = Drawing(DWGW, DWGW, name="straps.png", bg=(.8, .8, .8))
 
+with dwg.style(rgb=(1, 1, 1)):
+    for strap in straps:
+        replay_path(strap.sides[0], dwg)
+        replay_path(strap.sides[1][::-1], dwg, start=False)
+        dwg.close_path()
+        dwg.fill()
 
-    dwg = Drawing(*size)
-    draw_it(TILEW, dwg, fat=False, color=(.8, .8, .8))
+with dwg.style(rgb=(0, 0, 0), width=2):
+    for strap in straps:
+        for side in strap.sides:
+            replay_path(side, dwg)
+            dwg.stroke()
 
-    pt = PathTiler()
-    draw = ThreeStarsDesign(TILEW)
-    pt.tile_p6m(draw.draw_triangle, dwg.get_size(), TILEW)
-    with dwg.style(rgb=(1, 0, 0), width=2, dash=[5, 5]):
-        pt.replay_paths(dwg)
-        dwg.stroke()
-
-    pt = PathTiler()
-    pt.translate(2 * TILEW * SQRT3 / 2, TILEW)
-    pt.reflect_xy(0, 0)
-    draw.draw_tile(pt)
-    with dwg.style(rgb=(0, 0, 0), width=6):
-        pt.replay_paths(dwg)
-        dwg.stroke()
-
-    dwg.write_to_png('three_stars_2_lined.png')
-
-
-    dwg = Drawing(*size)
-    draw_it(TILEW, dwg, fat=False, color=random_color, combined=False, line_width=8)
-    dwg.write_to_png('three_stars_3_chaos.png')
-
-
-    dwg = Drawing(*size)
-    draw_it(TILEW, dwg, fat=False, color=random_color, combined=True, line_width=8)
-    dwg.write_to_png('three_stars_4_joined.png')
-
-#talk_pictures()
-
-def final():
-    TILEW = int(DWGW/5)
-
-    dwg = Drawing(DWGW, DWGW, bg=(.85, .85, .85))
-    draw_it(TILEW, dwg)
-    dwg.write_to_png('three_stars_final.png')
-
-#final()
-
-
-if 1:
-    TILEW = int(DWGW/3)
-    if 0:
-        strap_kwargs = dict(width=TILEW / 60, random_factor=4.9)
-    else:
-        strap_kwargs = dict(width=TILEW / 15, random_factor=0)
-
-    pt = PathTiler()
-    draw = ThreeStarsDesign(TILEW)
-    draw.draw(pt, (DWGW, DWGW), TILEW)
-    paths = pt.paths
-    paths = combine_paths(pt.paths)
-    paths = [tuple(path) for path in paths]
-
-    straps = strapify(paths, **strap_kwargs)
-
-    dwg = Drawing(DWGW, DWGW, name="straps.png", bg=(.8, .8, .8))
-
-    with dwg.style(rgb=(1, 1, 1)):
-        for strap in straps:
-            replay_path(strap.sides[0], dwg)
-            replay_path(strap.sides[1][::-1], dwg, start=False)
-            dwg.close_path()
-            dwg.fill()
-
-    with dwg.style(rgb=(0, 0, 0), width=2):
-        for strap in straps:
-            for side in strap.sides:
-                replay_path(side, dwg)
-                dwg.stroke()
-
-    dwg.write()
+dwg.write()
